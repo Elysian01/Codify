728601909ed0315d9b39b379460c5e53e10d9c61
Abhishek Gupta <abhig0209@gmail.com>
2021-11-12 02:35:30 +0530

    demo model template created

--- codify/data/create_codebase.py
+++ codify/data/create_codebase.py
@@ -73,52 +73,168 @@ data = [
     },
     {
         "id": 5,
-        "intent": "null_imputation",
-        "entity_name": "STATISTICS",
-        "entity_value": "mean",
+        "intent": "csv_pandas",
+        "entity_name": "",
+        "entity_value": "",
         "priority": 1,
         "code": """
-        
+        import pandas as pd
+        df = pd.read_csv("./data.csv", encoding = "utf-8")
         """
     },
     {
         "id": 6,
-        "intent": "null_imputation",
-        "entity_name": "STATISTICS",
-        "entity_value": "mean",
+        "intent": "train_test_split",
+        "entity_name": "",
+        "entity_value": "",
         "priority": 1,
         "code": """
+        from sklearn.model_selection import train_test_split
         
+        X_train, X_test, y_train, y_test = train_test_split(X,y, test_size = 0.2, random_state = 41)
+
+        print("Shape of X train = ",X_train.shape)
+        print("Shape of y train = ",y_train.shape)
+        print("Shape of X test = ",X_test.shape)
+        print("Shape of y test = ",y_test.shape)
         """
     },
     {
         "id": 7,
-        "intent": "null_imputation",
-        "entity_name": "STATISTICS",
-        "entity_value": "mean",
+        "intent": "classification",
+        "entity_name": "AMOUNT",
+        "entity_value": "all",
         "priority": 1,
         "code": """
-        
+        from sklearn.naive_bayes import GaussianNB
+        from sklearn.tree import DecisionTreeClassifier
+        from sklearn.ensemble import RandomForestClassifier
+        from sklearn.linear_model import LogisticRegression
+        from sklearn.neighbors import KNeighborsClassifier
+        from sklearn.svm import SVC
+
+
+        model_dict = {1: "Support Vector Machine", 2: "Decision Tree",
+                    3: "Random Forest", 4: "KNN", 5: "Naive Bayes"}
+
+
+        def model(X_train, y_train, X_test, y_test):
+
+            # Logistic Regression
+            #     logistic_classifier = LogisticRegression()
+            #     logistic_classifier.fit(X_train,y_train)
+            #     logistic_classifier_score = logistic_classifier.score(X_test, y_test)
+            #     print("Logistic Regression Accuracy: ", logistic_classifier_score * 100)
+
+            # Support Vector Classification
+            sv_classifier = SVC(kernel="rbf")
+            sv_classifier.fit(X_train, y_train)
+            sv_classifier_score = sv_classifier.score(X_test, y_test)
+            print("Support Vector Machine Accuracy: ", sv_classifier_score * 100)
+
+            # Decision Tree
+            dt_classifier = DecisionTreeClassifier(criterion="gini")
+            dt_classifier.fit(X_train, y_train)
+            dt_classifier_score = dt_classifier.score(X_test, y_test)
+            print("Decision Tree Accuracy: ", dt_classifier_score * 100)
+
+            # Random Forest
+            rf_classifier = RandomForestClassifier(n_estimators=100, criterion="gini")
+            rf_classifier.fit(X_train, y_train)
+            rf_classifier_score = rf_classifier.score(X_test, y_test)
+            print("Random Forest Accuracy: ", rf_classifier_score * 100)
+
+            # KNN
+            knn_classifier = KNeighborsClassifier(n_neighbors=5)
+            knn_classifier.fit(X_train, y_train)
+            knn_classifier_score = knn_classifier.score(X_test, y_test)
+            print("KNN Accuracy: ", knn_classifier_score * 100)
+
+            # Naive Bayes
+            naive_classifier = GaussianNB()
+            naive_classifier.fit(X_train, y_train)
+            naive_classifier_score = naive_classifier.score(X_test, y_test)
+            print("Naive Bayes Accuracy: ", naive_classifier_score * 100)
+
+            return sv_classifier, dt_classifier, rf_classifier, knn_classifier, naive_classifier
+
+
+        models = model(X_train, y_train, X_test, y_test)
+
+
         """
     },
     {
         "id": 8,
-        "intent": "null_imputation",
+        "intent": "describe_data",
         "entity_name": "STATISTICS",
-        "entity_value": "mean",
+        "entity_value": "statistical",
         "priority": 1,
         "code": """
+            from IPython.display import display
+            
+            def columns_type(df) -> tuple:
+                '''
+                return number of categorical and numerical columns
+                '''
+                numerics = ['int16', 'int32', 'int64', 'float16', 'float32', 'float64']
+                num_cols = df.select_dtypes(include=numerics).columns.tolist()
+                cat_columns = len(df.select_dtypes(include="O").columns.tolist())
+                num_columns = len(num_cols)
+                return (cat_columns, num_columns)
         
+            def null_columns_percentage(df) -> pd.DataFrame:
+                '''
+                Prints Null Information of dataframe,i.e. only the number of rows having null values and their null percentage
+                '''
+                print("\nNull Information of Dataframe: \n")
+                null_df = pd.DataFrame(df.isnull().sum()).reset_index()
+                null_df.columns = ["column_name", "null_rows"]
+                null_df["null_percentage"] = null_df["null_rows"]*100 / df.shape[0]
+                null_df = null_df[null_df["null_percentage"] != 0].sort_values(
+                    "null_percentage", ascending=False).reset_index(drop=True)
+                print(
+                    f"\nThere are total {null_df.shape[0]} columns having null values out of {df.shape[1]} columns in dataframe\n")
+                display(null_df)
+                return null_df
+                
+            def statistics(df) -> None:
+                '''
+                Gives number of categorical and numerical columns, description and info regarding the dataframe
+                '''
+                attribute_types = columns_type(df)
+                print(
+                    f"\nThere are total {attribute_types[0]} categorical and {attribute_types[1]} numerical columns\n")
+
+                print("Description of Data:\n")
+                display(df.describe())
+
+                print("Information regarding data: \n")
+                display(df.info())
+
+                if df.isnull().sum().sum() > 0:
+                    null_columns_percentage(df)
+                else:
+                    print(
+                        "\nCongrats!!, The Dataframe has NO NULL VALUES\n")
         """
     },
     {
         "id": 9,
-        "intent": "null_imputation",
-        "entity_name": "STATISTICS",
-        "entity_value": "mean",
+        "intent": "classify_eval",
+        "entity_name": "evaluation",
+        "entity_value": "classification",
         "priority": 1,
         "code": """
-        
+        from sklearn.metrics import classification_report
+        from sklearn.metrics import accuracy_score
+
+
+        for i in range(len(model)):
+            print("Model ", i)
+            print(classification_report(y_test,model[i].predict(X_test)))
+            print(accuracy_score(y_test,model[i].predict(X_test)))
+            print()
         """
     },
     {
